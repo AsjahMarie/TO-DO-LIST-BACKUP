8a92b54ed32c8e2f78ee27453beff2723dd22708
Asjah Marie <117318989+AsjahMarie@users.noreply.github.com>
2023-11-06 16:59:48 -0600

    Update script.js
    calendar js code added

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ function showLoginForm() {
     document.getElementById('signupForm').classList.remove('active');
     document.getElementById('loginForm').classList.add('active');
 }
+
+//for Calendar
+document.addEventListener('DOMContentLoaded', function() {
+    var calendar = $('#calendar').fullCalendar({
+        // Existing options...
+
+        // Add a callback function for eventDrop (if you want to allow event moving)
+        eventDrop: function(event) {
+            alert(event.title + ' was dropped on ' + event.start.format());
+            // Handle event drop action (e.g., update event on drop)
+        }
+    });
+});
+
