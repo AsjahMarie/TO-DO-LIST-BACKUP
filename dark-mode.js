2e39ae9617aa7817bc465d1e7cafc7d89a1eb356
ISanchez14 <137968230+ISanchez14@users.noreply.github.com>
2023-11-16 02:29:31 -0600

    Update dark-mode.js

--- dark-mode.js
+++ dark-mode.js
@@ -26,6 +26,6 @@ function darkmode() {
     element.classList.toggle('dark-mode', !wasDark);
 }
 
-function onload(){
+function onclick(){
     document.body.classList.toggle('dark-mode', localStorage.getItem('darkmode') == 'true');
 }
