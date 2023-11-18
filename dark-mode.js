c883845681f209c4207f666e98d3025706cfdb9c
Emma Bagazinski <ebagazinski@twu.edu>
2023-11-17 19:53:18 -0600

    editing darkMode with localStorage

--- dark-mode.js
+++ dark-mode.js
@@ -1,4 +1,5 @@
 // dark-mode.js
+/*
 function changeTheme() {
     const darkModeRadio = document.getElementById('darkModeRadio');
 
@@ -12,4 +13,28 @@ function changeTheme() {
 const darkModeRadio = document.getElementById('darkModeRadio');
 if (darkModeRadio) {
     darkModeRadio.addEventListener('change', changeTheme);
+}
+*/
+document.addEventListener('DOMContentLoaded', function(){
+    var themeMode = localStorage.getItem('modeCheckbox') === 'true';
+    var bodyTheme = localStorage.getItem()
+
+    document.getElementById('darkMode').checked = themeMode;
+});
+
+function changeMode(){
+    const body = document.body;
+    const checkbox = document.getElementById('darkMode');
+
+    if(checkbox.checked){
+        body.classList.remove('light-mode');
+        body.classList.add('dark-mode');
+    } else {
+        body.classList.remove('dark-mode');
+        body.classList.add('light-mode');
+    }
+
+    localStorage.setItem('modeCheckbox', checkbox.checked);
+    
+
 }
