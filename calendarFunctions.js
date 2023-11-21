054b39ee8ecab7a7340d377ef7c52205d70a998d
emmabagazinski <144265552+emmabagazinski@users.noreply.github.com>
2023-11-20 21:42:48 -0600

    adding task synchronization

---
+++ calendarFunctions.js
@@ -0,0 +1,449 @@
+// CALENDAR FUNCTIONS
+
+const calendar = document.querySelector(".calendar"),
+  date = document.querySelector(".date"),
+  daysContainer = document.querySelector(".days"),
+  prev = document.querySelector(".prev"),
+  next = document.querySelector(".next"),
+  todayBtn = document.querySelector(".today-btn"),
+  gotoBtn = document.querySelector(".goto-btn"),
+  dateInput = document.querySelector(".date-input"),
+  taskDay = document.querySelector(".task-day"),
+  taskDate = document.querySelector(".task-date"),
+  tasksContainer = document.querySelector(".tasks"),
+  addTaskBtn = document.querySelector(".add-task"),
+  addTaskWrapper = document.querySelector(".add-task-wrapper "),
+  addTaskCloseBtn = document.querySelector(".close "),
+  addTaskTitle = document.querySelector(".task-name "),
+  addTaskFrom = document.querySelector(".task-time-from "),
+  addTaskTo = document.querySelector(".task-time-to "),
+  addTaskSubmit = document.querySelector(".add-task-btn ");
+
+let today = new Date();
+let activeDay;
+let month = today.getMonth();
+let year = today.getFullYear();
+
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const tasksArr = [];
+//getTasks();
+
+//function to add days in days with class day and prev-date next-date on previous month and next month days and active on today
+function initCalendar() {
+  const firstDay = new Date(year, month, 1);
+  const lastDay = new Date(year, month + 1, 0);
+  const prevLastDay = new Date(year, month, 0);
+  const prevDays = prevLastDay.getDate();
+  const lastDate = lastDay.getDate();
+  const day = firstDay.getDay();
+  const nextDays = 7 - lastDay.getDay() - 1;
+
+  date.innerHTML = months[month] + " " + year;
+
+  let days = "";
+
+  for (let x = day; x > 0; x--) {
+    days += `<div class="day prev-date">${prevDays - x + 1}</div>`;
+  }
+
+  for (let i = 1; i <= lastDate; i++) {
+    //check if task is present on that day
+    let task = false;
+    tasksArr.forEach((taskObj) => {
+      if (
+        taskObj.day === i &&
+        taskObj.month === month + 1 &&
+        taskObj.year === year
+      ) {
+        task = true;
+      }
+    });
+    if (
+      i === new Date().getDate() &&
+      year === new Date().getFullYear() &&
+      month === new Date().getMonth()
+    ) {
+      activeDay = i;
+      getActiveDay(i);
+      //updateTasks(i);
+      if (task) {
+        days += `<div class="day today active task">${i}</div>`;
+      } else {
+        days += `<div class="day today active" id="active">${i}</div>`;
+      }
+    } else {
+      if (task) {
+        days += `<div class="day task">${i}</div>`;
+      } else {
+        days += `<div class="day ">${i}</div>`;
+      }
+    }
+  }
+
+  for (let j = 1; j <= nextDays; j++) {
+    days += `<div class="day next-date">${j}</div>`;
+  }
+  daysContainer.innerHTML = days;
+  addListener();
+}
+
+//function to add month and year on prev and next button
+function prevMonth() {
+  month--;
+  if (month < 0) {
+    month = 11;
+    year--;
+  }
+  initCalendar();
+}
+
+function nextMonth() {
+  month++;
+  if (month > 11) {
+    month = 0;
+    year++;
+  }
+  initCalendar();
+}
+
+prev.addEventListener("click", prevMonth);
+next.addEventListener("click", nextMonth);
+
+initCalendar();
+
+//function to add active on day
+function addListener() {
+  const days = document.querySelectorAll(".day");
+  days.forEach((day) => {
+    day.addEventListener("click", (e) => {
+      getActiveDay(e.target.innerHTML);
+      //updateTasks(Number(e.target.innerHTML));
+      activeDay = Number(e.target.innerHTML);
+      //remove active
+      days.forEach((day) => {
+        day.removeAttribute('id','active');
+      });
+      //if clicked prev-date or next-date switch to that month
+      if (e.target.classList.contains("prev-date")) {
+        prevMonth();
+        //add active to clicked day afte month is change
+        setTimeout(() => {
+          //add active where no prev-date or next-date
+          const days = document.querySelectorAll(".day");
+          days.forEach((day) => {
+            if (
+              !day.classList.contains("prev-date") &&
+              day.innerHTML === e.target.innerHTML
+            ) {
+              day.setAttribute('id','active');
+            }
+          });
+        }, 100);
+      } else if (e.target.classList.contains("next-date")) {
+        nextMonth();
+        //add active to clicked day after month is changed
+        setTimeout(() => {
+          const days = document.querySelectorAll(".day");
+          days.forEach((day) => {
+            if (
+              !day.classList.contains("next-date") &&
+              day.innerHTML === e.target.innerHTML
+            ) {
+              day.setAttribute('id','active');
+            }
+          });
+        }, 100);
+      } else {
+        e.target.setAttribute('id', 'active');
+      }
+    });
+  });
+}
+
+todayBtn.addEventListener("click", () => {
+  today = new Date();
+  month = today.getMonth();
+  year = today.getFullYear();
+  initCalendar();
+});
+
+dateInput.addEventListener("input", (e) => {
+  dateInput.value = dateInput.value.replace(/[^0-9/]/g, "");
+  if (dateInput.value.length === 2) {
+    dateInput.value += "/";
+  }
+  if (dateInput.value.length > 7) {
+    dateInput.value = dateInput.value.slice(0, 7);
+  }
+  if (e.inputType === "deleteContentBackward") {
+    if (dateInput.value.length === 3) {
+      dateInput.value = dateInput.value.slice(0, 2);
+    }
+  }
+});
+
+gotoBtn.addEventListener("click", gotoDate);
+
+function gotoDate() {
+  const dateArr = dateInput.value.split("/");
+  if (dateArr.length === 2) {
+    if (dateArr[0] > 0 && dateArr[0] < 13 && dateArr[1].length === 4) {
+      month = dateArr[0] - 1;
+      year = dateArr[1];
+      initCalendar();
+      return;
+    }
+  }
+  alert("Invalid Date");
+}
+
+//function get active day day name and date and update taskday taskdate
+function getActiveDay(date) {
+  const day = new Date(year, month, date);
+  const dayName = day.toString().split(" ")[0];
+  taskDay.innerHTML = dayName;
+  taskDate.innerHTML = date + " " + months[month] + " " + year;
+  console.log('active day: ' + taskDate.innerHTML);
+  return taskDate;
+}
+
+getActiveDay(today);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //function update tasks when a day is active
+// function updateTasks(date) {
+//   let tasks = "";
+//   const taskForDate = tasksArr.find(task => task.day === date && task.month 
+//     === month + 1 && task.year === year);
+//     if (taskForDate && taskForDate.tasks.length > 0){
+//     } else {
+//       tasks = '<div class = "no task"><h3>No Tasks</h3></div>';
+//     }
+//     tasksContainer.innerHTML = "";
+//     tasksContainer.innerHTML = tasks;
+//     saveTasks();
+// }
+
+// //function to add task
+// addTaskBtn.addEventListener("click", () => {
+//   addTaskWrapper.classList.toggle("active");
+// });
+
+// // addTaskCloseBtn.addEventListener("click", () => {
+// //   addTaskWrapper.classList.remove("active");
+// // });
+
+// document.addEventListener("click", (e) => {
+//   if (e.target !== addTaskBtn && !addTaskWrapper.contains(e.target)) {
+//     addTaskWrapper.classList.remove("active");
+//   }
+// });
+
+//allow 50 chars in tasktitle
+// addTaskTitle.addEventListener("input", (e) => {
+//   addTaskTitle.value = addTaskTitle.value.slice(0, 60);
+// });
+
+// Allow only time in tasktime from and to
+// addTaskFrom.addEventListener("input", (e) => {
+//   addTaskFrom.value = addTaskFrom.value.replace(/[^0-9:]/g, "");
+//   if (addTaskFrom.value.length === 2 && e.inputType !== "deleteContentBackward") {
+//     addTaskFrom.value += ":";
+//   }
+//   if (addTaskFrom.value.length > 5) {
+//     addTaskFrom.value = addTaskFrom.value.slice(0, 5);
+//   }
+// });
+
+// addTaskTo.addEventListener("input", (e) => {
+//   addTaskTo.value = addTaskTo.value.replace(/[^0-9:]/g, "");
+//   if (addTaskTo.value.length === 2 && e.inputType !== "deleteContentBackward") {
+//     addTaskTo.value += ":";
+//   }
+//   if (addTaskTo.value.length > 5) {
+//     addTaskTo.value = addTaskTo.value.slice(0, 5);
+//   }
+// });
+
+
+//function to add task to tasksArr
+// addTaskSubmit.addEventListener("click", () => {
+//   const taskTitle = addTaskTitle.value;
+//   const taskTimeFrom = addTaskFrom.value;
+//   const taskTimeTo = addTaskTo.value;
+//   const taskColor = addTaskWrapper.querySelector(".task-color").value;
+//   const subtask = addTaskWrapper.querySelector(".subtask").value;
+//   const shareTask = addTaskWrapper.querySelector(".share-task").value;
+//   if (taskTitle === "" || taskTimeFrom === "" || taskTimeTo === "") {
+//     alert("Please fill all the fields");
+//     return;
+
+  // updateTasks(activeDay);
+  // initCalendar();
+  // }
+
+
+  // //check if task is already added
+  // let taskExist = false;
+  // tasksArr.forEach((task) => {
+  //   if (
+  //     task.day === activeDay &&
+  //     task.month === month + 1 &&
+  //     task.year === year
+  //   ) {
+  //     task.tasks.forEach((task) => {
+  //       if (task.title === taskTitle) {
+  //         taskExist = true;
+  //       }
+  //     });
+  //   }
+  // });
+  // if (taskExist) {
+  //   alert("Task already added");
+  //   //return;
+  // }
+  // const newTask = {
+  //   title: taskTitle,
+  //   color: taskColor,
+  //   //subtask: subtask,
+  //   //shareTask: shareTask,
+  // };
+  // console.log(newTask);
+  // console.log(activeDay);
+  // let taskAdded = false;
+  // if (tasksArr.length > 0) {
+  //   tasksArr.forEach((item) => {
+  //     if (
+  //       item.day === activeDay &&
+  //       item.month === month + 1 &&
+  //       item.year === year
+  //     ) {
+  //       item.tasks.push(newTask);
+  //       taskAdded = true;
+  //     }
+  //   });
+  // }
+
+  // if (!taskAdded) {
+  //   tasksArr.push({
+  //     day: activeDay,
+  //     month: month + 1,
+  //     year: year,
+  //     tasks: [newTask],
+  //   });
+  // }
+
+  //addTaskWrapper.classList.remove("active");
+  // addTaskTitle.value = "";
+  // addTaskFrom.value = "";
+  // addTaskTo.value = "";
+  // updateTasks(activeDay);
+  // //select active day and add task class if not added
+  // const activeDayEl = document.querySelector(".day.active");
+  // if (!activeDayEl.classList.contains("task")) {
+  //   activeDayEl.classList.add("task");
+  // }
+
+
+// //function to delete task when clicked on task
+// tasksContainer.addEventListener("click", (e) => {
+//   if (e.target.classList.contains("task")) {
+//     if (confirm("Are you sure you want to delete this task?")) {
+//       const taskTitle = e.target.children[0].children[1].innerHTML;
+//       tasksArr.forEach((task) => {
+//         if (
+//           task.day === activeDay &&
+//           task.month === month + 1 &&
+//           task.year === year
+//         ) {
+//           task.tasks.forEach((item, index) => {
+//             if (item.title === taskTitle) {
+//               task.tasks.splice(index, 1);
+//             }
+//           });
+//           //if no tasks left in a day then remove that day from tasksArr
+//           if (task.tasks.length === 0) {
+//             tasksArr.splice(tasksArr.indexOf(task), 1);
+//             //remove task class from day
+//             const activeDayEl = document.querySelector(".day.active");
+//             if (activeDayEl.classList.contains("task")) {
+//               activeDayEl.classList.remove("task");
+//             }
+//           }
+//         }
+//       });
+//       updateTasks(activeDay);
+//     }
+//   }
+// });
+
+// //function to save tasks in local storage
+// function saveTasks() {
+//   if (tasksArr.length != 0){
+//     localStorage.setItem("tasks", JSON.stringify(tasksArr));
+//   }
+// }
+
+// //function to get tasks from local storage
+// // function getTasks() {
+// //   const storedTasks=JSON.parse(localStorage.getItem("tasks"));
+// //   if (storedTasks && Array.isArray(storedTasks)&& storedTasks.length > 0){
+// //     tasksArr.push(...storedTasks);
+// //   }
+// // }
+
+// function convertTime(time) {
+//   //convert time to 24 hour format
+//   let timeArr = time.split(":");
+//   let timeHour = timeArr[0];
+//   let timeMin = timeArr[1];
+//   let timeFormat = timeHour >= 12 ? "PM" : "AM";
+//   timeHour = timeHour % 12 || 12;
+//   time = timeHour + ":" + timeMin + " " + timeFormat;
+//   return time;
+// }
---
+++ taskFunctions.js
@@ -0,0 +1,353 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const divSection = document.getElementById('divSection');
+    const taskList = document.getElementById('tasks');
+    divSection.style.display = 'none';
+});
+
+function composeEmail(event){
+    event.preventDefault();
+
+    /*Subtask inputs and variables*/
+    const subInput = document.getElementById('subtaskInput');
+    const subDueDate = document.getElementById('subtaskdueDateInput').value;
+    const subTask = document.getElementById('subTask');
+    /*main task inputs and variables*/
+    const dueDate = document.getElementById('dueDateInput').value;
+    const emailAddress = document.getElementById('emailAddress').value;
+    const task = document.getElementById('taskInput');
+    if(subTask.checked){
+        const subject = "Sharing my tasks with you!";
+        const encodedSubject = encodeURIComponent(subject);
+
+        const subInputValue = subInput.value;
+        const taskValue = task.value;
+        const body = "my task: '"+ taskValue + "'' is due on: " + dueDate + " and my subtask: '" + subInputValue + "'' is due on: " + subDueDate;
+        window.open(`mailto:${emailAddress}?subject=${encodedSubject}&body=${body}`);
+    } else {
+        const subject = "Sharing my task with you!";
+        const encodedSubject = encodeURIComponent(subject);
+        const taskValue = task.value;
+
+        const body = "my task: "+ taskValue + "  is due on: " + dueDate;
+        window.open(`mailto:${emailAddress}?subject=${encodedSubject}&body=${body}`);
+    }
+
+
+}
+
+function formatDate(){
+    const date = document.getElementById('dueDateInput').value;
+    // date is in YYYY-MM-DD format, want it to be 19 November 2023 to match Calendar 
+    const separateDate = date.split('-');
+
+    // now YYYY is in separateDate[0], MM is in separateDate[1], and DD is in separateDate[2]
+
+    // month array
+    const monArray = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    // value of separateDate[1] is the month in monArray - 1 
+    const monValue = separateDate[1] - 1;
+
+    const dueDate = (separateDate[2] + " " + monArray[monValue] + " " + separateDate[0]);
+
+    return dueDate;
+}
+const tasks = document.getElementById('tasks');
+// add task to page as li within ul
+function addTask(){
+
+    // input variables
+    const taskInput = document.getElementById('taskInput');
+    const tasks = document.getElementById('tasks');
+    const taskColor = document.getElementById('taskColorPicker').value;
+    const dueDate = formatDate();
+
+    // if task is not empty
+    if (taskInput.value.trim() !== ''){
+        // remove and edit buttons to add to li
+        const deleteBtn = "<button onclick='deleteTask(this)' id='deleteBtn'>complete & remove</button>";
+        const editBtn = "<button onclick='editTask(this)' id='editBtn'>edit task</button>";
+        const taskValue = taskInput.value;
+
+        // add elements to li tag
+        const li = document.createElement('li');
+        li.textContent = taskValue + (dueDate ? ` (Due: ${dueDate})` : '');
+        li.innerHTML += `${deleteBtn}`;
+        li.innerHTML += `${editBtn}`;
+        li.style.backgroundColor = taskColor;
+        li.dataset.taskName = taskValue;
+        li.dataset.taskColor = taskColor;
+        li.dataset.dueDate = dueDate;
+        // append li to ul
+        tasks.appendChild(li);
+
+        // reset taskInput value
+        taskInput.value='';
+        
+    } else {
+        alert("please enter a task")
+    }
+
+    // save page and reload to get rid of task form
+    saveTasks();
+    //location.reload();
+}
+
+// function saveArray(){
+//     // get all li of #tasks and store to array
+//     const nodeList = document.querySelectorAll('#tasks li');
+//     const taskArray = Array.from(nodeList);
+//     console.log(taskArray);
+
+//     // save array to localStorage
+//     window.localStorage.setItem("array", JSON.stringify(taskArray));
+// }
+
+/*function that gets delete button's parent node (li) and deletes it from it's parent node (ul)*/
+function deleteTask(dBtn){
+    var taskListed = dBtn.parentNode;
+    taskListed.parentNode.removeChild(taskListed);
+    saveTasks();
+}
+
+function editTask(eBtn){
+    // get parent of edit button -> li of task
+    const taskListed = eBtn.parentNode;
+    // show task form
+    const divSection = document.getElementById('divSection');
+    divSection.style.display = 'block';
+
+    // first check if there is a subtask and add values from there
+    // if not only add values from main task
+    if(taskListed.querySelector('ul') !== null) {
+        // show subtask form
+        const subCheck = document.getElementById('subTask');
+        const addTaskBtn = document.getElementById('taskButton');
+        const subTaskForm = document.getElementById('subTaskForm');
+        subTaskForm.classList.remove('hiddenElement');
+        subTaskForm.classList.add('displayGrid');
+        addTaskBtn.classList.add('hiddenElement');
+        subCheck.checked = true;
+
+        // get ul of task, then li of ul (subtask)
+        const subList = taskListed.querySelector('ul');
+        const subTask = subList.querySelector('li');
+
+        // get values of saved subtask
+        const subInput = subTask.getAttribute('data-task-name');
+        const subColor = subTask.getAttribute('data-task-color');
+        const subDate = subTask.getAttribute('data-due-date');
+        // get input locations for subtask
+        const subTaskInput = document.getElementById('subtaskInput');
+        const subTaskColor = document.getElementById('subtaskColorPicker');
+        const subDueDate = document.getElementById('subtaskdueDateInput');
+        // fill in subtask input fields with saved subtask to edit
+        subTaskInput.value = subInput;
+        subTaskColor.value = subColor;
+        subDueDate.value = subDate
+
+        // get values of saved task
+        const oldInput = taskListed.getAttribute('data-task-name');
+        const oldColor = taskListed.getAttribute('data-task-color');
+        const oldDate = taskListed.getAttribute('data-due-date');
+        // get input locations for main task
+        const taskInput = document.getElementById('taskInput');
+        const taskColor = document.getElementById('taskColorPicker');
+        const dueDate = document.getElementById('dueDateInput');
+        // fill in input values with task to edit
+        taskInput.value = oldInput;
+        taskColor.value = oldColor;
+        dueDate.value = oldDate;
+
+        // remove old task and subtask and save
+        taskListed.remove();
+
+        // new edited tasks will be added with functions from task form
+        // save task to remove old versions of tasks
+        saveTasks();
+
+    } else {
+        // get values of saved task
+        const oldInput = taskListed.getAttribute('data-task-name');
+        const oldColor = taskListed.getAttribute('data-task-color');
+        const oldDate = taskListed.getAttribute('data-due-date');
+        // get input locations for main task
+        const taskInput = document.getElementById('taskInput');
+        const taskColor = document.getElementById('taskColorPicker');
+        const dueDate = document.getElementById('dueDateInput');
+        // fill in input values with task to edit
+        taskInput.value = oldInput;
+        taskColor.value = oldColor;
+        dueDate.value = oldDate;
+
+        // remove old task and save
+        taskListed.remove();
+        saveTasks();
+    }
+    
+
+}
+
+// save tasks to local storage
+function saveTasks(){
+    window.localStorage.setItem("tasks", tasks.innerHTML)
+
+    location.reload();
+}
+
+// get tasks from local storage 
+function displayTasks(){
+    if(tasks){
+        tasks.innerHTML = window.localStorage.getItem("tasks");
+    }
+    
+}
+
+// erase tasks from localStorage
+function clearAll(){
+    window.localStorage.removeItem("tasks");
+    location.reload();
+}
+
+// call function to show tasks
+displayTasks();
+
+function submitForm(){
+    const checkBox = document.getElementById('collaborate');
+    const subTask = document.getElementById('subTask');
+
+    if (subTask.checked && checkBox.checked){
+        composeEmail(event);
+        console.log('i went to the submit place');
+        addSubtaskTask();
+        location.reload();
+    } else if(checkBox.checked){
+        composeEmail(event);
+        addTask();
+        location.reload();
+    } else {
+        addTask();
+        location.reload();
+    }
+}
+
+function displayHidden(){
+    const checkBox = document.getElementById('collaborate');
+    const subBox = document.getElementById('subTask');
+    const shareForm = document.getElementById('shareForm');
+    const addTaskBtn = document.getElementById('taskButton');
+    const addSubBtn = document.getElementById('subtaskButton');
+
+    if(checkBox.checked){
+        shareForm.classList.remove('hiddenElement');
+        shareForm.classList.add('displayGrid');
+        addTaskBtn.classList.add('hiddenElement');
+        addSubBtn.classList.add('hiddenElement');
+    } else if (!checkBox.checked && subBox.checked){
+        addTaskBtn.classList.add('hiddenElement');
+        shareForm.classList.add('hiddenElement');
+        addSubBtn.classList.remove('hiddenElement');
+    } else {
+        shareForm.classList.add('hiddenElement');
+        shareForm.classList.remove('displayGrid');
+        addTaskBtn.classList.remove('hiddenElement');
+        addSubBtn.classList.remove('hiddenElement');
+    }
+}
+
+function removeTaskFromCalendar(taskName) {
+    const taskData = {
+        type: 'removeTask',
+        name: taskName
+    };
+    window.postMessage(taskData, '*');
+}
+
+function goToAddTaskPage() {
+    window.location.href = 'addTaskPage.html';
+}
+
+function showSubtaskForm(){
+    const subCheck = document.getElementById('subTask');
+    const addTaskBtn = document.getElementById('taskButton');
+    const subTaskForm = document.getElementById('subTaskForm');
+
+    if(subCheck.checked){
+        subTaskForm.classList.remove('hiddenElement');
+        subTaskForm.classList.add('displayGrid');
+        addTaskBtn.classList.add('hiddenElement');
+    } else {
+        subTaskForm.classList.add('hiddenElement');
+        subTaskForm.classList.remove('displayGrid');
+        addTaskBtn.classList.remove('hiddenElement');
+    }
+}
+
+function addSubtaskTask(){
+    /*Main task variables*/
+    const taskInput = document.getElementById('taskInput');
+    const tasks = document.getElementById('tasks');
+    const taskColor = document.getElementById('taskColorPicker').value;
+    const dueDate = document.getElementById('dueDateInput').value;
+    const li = document.createElement('li');
+    /*Sub task variables*/
+    const subInput = document.getElementById('subtaskInput');
+    const subDueDate = document.getElementById('subtaskdueDateInput').value;
+    const subColor = document.getElementById('subtaskColorPicker').value;
+    const subCheck = document.getElementById('subTask');
+
+    if (taskInput.value.trim() !== '' && subInput.value.trim() !== ''){
+        const deleteBtn = "<button onclick='deleteTask(this)' id='deleteBtn'>complete & remove</button>";
+        const editBtn = "<button onclick='editTask(this)' id='editBtn'>edit task</button>";
+        const taskValue = taskInput.value;
+        const subValue = subInput.value;
+
+        li.textContent = taskValue + (dueDate ? ` (Due: ${dueDate})` : '');
+        li.innerHTML += `${deleteBtn}`;
+        li.innerHTML += `${editBtn}`;
+        li.style.backgroundColor = taskColor;
+        li.dataset.taskName = taskValue;
+        li.dataset.taskColor = taskColor;
+        li.dataset.dueDate = dueDate;
+
+        const subul = document.createElement('ul');
+        const subli = document.createElement('li');
+        subli.textContent = subValue + (subDueDate ? ` (Due: ${subDueDate})` : '');
+        subli.innerHTML += `${deleteBtn}`;
+        subli.style.backgroundColor = subColor;
+        subli.dataset.taskName = subValue;
+        subli.dataset.taskColor = subColor;
+        subli.dataset.dueDate = subDueDate;
+        subul.appendChild(subli);
+        li.appendChild(subul);
+        tasks.appendChild(li);
+
+        taskInput.value='';
+        subInput.value='';
+        saveTasks();
+    
+    } else {
+        alert("please enter a task")
+    }
+
+    saveTasks();
+    location.reload();
+
+}
+
+function showTaskForm(){
+    const divSection = document.getElementById('divSection');
+    divSection.style.display = 'block';
+}
