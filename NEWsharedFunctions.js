af9d9f9115a28ceb7ce597272cad2131eb5d8018
Emma Bagazinski <ebagazinski@twu.edu>
2023-11-21 11:18:11 -0600

    fixing null error

--- NEWsharedFunctions.js
+++ NEWsharedFunctions.js
@@ -14,34 +14,38 @@ function displayCalTasks(){
     console.log(tasks);
 
     // split oldArray where there is an "li" element 
-    var oldArray = tasks.split(/<\/?li>/);
-    console.log(oldArray);
+    if(tasks === null){
+        //do nothing
+    } else {
+        var oldArray = tasks.split(/<\/?li>/);
+        console.log(oldArray);
 
-    // get rid of any empty values in array and add to new taskArray
-    const taskArray = oldArray.filter(function (task){
-        return task.trim() !== '';
-    })
-    console.log(taskArray);
+        // get rid of any empty values in array and add to new taskArray
+        const taskArray = oldArray.filter(function (task){
+            return task.trim() !== '';
+        })
+        console.log(taskArray);
 
-    // create new ul element to hold tasks for calendar screen page
-    var taskUl = document.createElement('ul');
-    taskUl.setAttribute('id','tasks');
+        // create new ul element to hold tasks for calendar screen page
+        var taskUl = document.createElement('ul');
+        taskUl.setAttribute('id','tasks');
 
-    // add li elements from array to ul to access attributes in later code
-    for (i=0; i<taskArray.length; i++){
-        taskUl.innerHTML += taskArray[i];
-    }
+        // add li elements from array to ul to access attributes in later code
+        for (i=0; i<taskArray.length; i++){
+            taskUl.innerHTML += taskArray[i];
+        }
 
 
-    console.log(taskUl);
+        console.log(taskUl);
 
-    // loop through ul and get dueDate attribute for each task, if it equals the active day display it on the screen
-    for (i=0; i<taskArray.length; i++){
-        if(taskUl.children[i].getAttribute('data-due-date') === currDay){
-            const ul = document.createElement('ul');
-            ul.setAttribute('id','tasks');
-            ul.appendChild(taskUl.children[i]);
-            dateLocation.innerHTML += ul.outerHTML;
+        // loop through ul and get dueDate attribute for each task, if it equals the active day display it on the screen
+        for (i=0; i<taskArray.length; i++){
+            if(taskUl.children[i].getAttribute('data-due-date') === currDay){
+                const ul = document.createElement('ul');
+                ul.setAttribute('id','tasks');
+                ul.appendChild(taskUl.children[i]);
+                dateLocation.innerHTML += ul.outerHTML;
+            }
         }
     }
 
